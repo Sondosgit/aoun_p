64295602097e835ef6bbe6a3b6a11a73069f1a1c
Sondosgit <132937887+Sondosgit@users.noreply.github.com>
2024-02-10 01:02:18 +0300

    new line

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        
+        //new line
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
