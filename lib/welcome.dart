cb167569d0df588b2f3aa1041ea227d234ad9320
ghadah <ghadah.ak.22@gmail.com>
2024-02-13 23:06:39 +0300

    welcome page

---
+++ lib/welcome.dart
@@ -0,0 +1,47 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/Onbording.dart';
+
+class Welcom extends StatefulWidget {
+  const Welcom({Key? key}) : super(key: key);
+
+  @override
+ 
+  // ignore: library_private_types_in_public_api
+  _WelcomState createState() => _WelcomState();
+}
+
+class _WelcomState extends State<Welcom> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Add a delay of 5 seconds (adjust the duration as needed)
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Onbording()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            child: Image.asset(
+              "img/welcom.jpg",
+              fit: BoxFit.cover,
+              width: double.infinity,
+              
+            ),
+          ),
+          // Add more widgets or content here if needed
+        ],
+      ),
+    );
+  }
+}
