4163b48bc0c6fe3f41b4634ec0afc9efc0c60df0
LamaaAlomran <147472491+LamaaAlomran@users.noreply.github.com>
2024-02-16 20:53:08 +0300

    Update main.dart
    Change main starter page

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
       ),
-      home: const SignUp(),
+      home: const Welcom(),
     );
   }
 }
