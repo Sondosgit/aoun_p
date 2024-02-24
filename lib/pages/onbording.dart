cef972f3d276990ad72048451e29e49fad8066e3
aseel-uni <442200868@student.ksu.edu.sa>
2024-02-24 23:17:44 +0300

    onbording page complet

--- lib/pages/onbording.dart
+++ lib/pages/onbording.dart
@@ -1,58 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:aoun_project/login.dart';
 
 class Onbording extends StatelessWidget {
   const Onbording({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "من أنت ؟",
-                textAlign: TextAlign.right,
-                textDirection: TextDirection.rtl,
-                style: TextStyle(
-                  color: Color(0xFF545454),
-                  fontSize: 37,
-                ),
-              ),
-              ClipOval(
-                child: Image.asset(
-                  "img/elderly.jpg",
-                  width: 150, // Adjust as needed
-                  height: 150, // Adjust as needed
-                ),
-              ),
-              const Text(
-                "كبير سن",
-                style: TextStyle(
-                  color: Color(0xFF545454),
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(height: 50),
-              ClipOval(
-                child: Image.asset(
-                  "img/cargiver.jpg",
-                  width: 150, // Adjust as needed
-                  height: 150, // Adjust as needed
-                ),
-              ),
-              const Text(
-                "مقدم رعاية",
-                style: TextStyle(
-                  color: Color(0xFF545454),
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return Container(
+        decoration:const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.center ,
+            end: Alignment.bottomCenter, 
+            colors: [Colors.white , Color.fromARGB(255, 181, 187, 253)])),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerRight,
+          child: Text("من أنت؟"  ,style: TextStyle(fontSize: 36 , color: Colors.black , fontWeight: FontWeight.bold) )
+        ), ),
+      body: Center( child : Column( children: [
+        Padding(padding: EdgeInsets.symmetric(vertical: 40)),
+        Material(
+          color: Color(0xFF8485EC),
+          elevation: 9,
+          shape: CircleBorder(),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child:InkWell(
+            splashColor: Colors.black26,
+            onTap: (){
+             Navigator.of(context).push(MaterialPageRoute
+                    (builder: (context) => LoginScreen()));
+
+            },
+           child: Ink.image(image: AssetImage("img/eld.png"),
+           height: 200,
+           width: 200,
+           fit: BoxFit.contain,
+          
+        ),)),
+        Text("كبير السن" ,style: TextStyle(fontSize: 30 , color: Colors.black , fontWeight: FontWeight.w100),),
+        Container(height:20 ,),
+        Material(
+          color: Color(0xFF8485EC),
+          elevation: 9,
+          shape: CircleBorder(),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child:InkWell(
+            splashColor: Colors.black26,
+            onTap: (){},
+           child: Ink.image(image: AssetImage("img/care.png"),
+           height: 200,
+           width: 200,
+           fit: BoxFit.contain,
+          
+        ),)),
+        Text("مقدم الرعاية", style: TextStyle(fontSize: 30 , color: Colors.black , fontWeight: FontWeight.w100),)
+
+      ],)) 
+      )
+     );
   }
 }
