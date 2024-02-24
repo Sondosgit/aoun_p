f3b33f02a738da1aa285fbb688cb3f78bd5aae0b
ghadah <ghadah.ak.22@gmail.com>
2024-02-24 15:08:09 +0300

    شكل الصور

--- lib/pages/onbording.dart
+++ lib/pages/onbording.dart
@@ -1,48 +1,56 @@
 import 'package:flutter/material.dart';
 
 class Onbording extends StatelessWidget {
-  const Onbording({super.key});
+  const Onbording({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Stack(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "من أنت ؟",
-                    textAlign: TextAlign.right,
-                    textDirection: TextDirection.rtl,
-                    style: TextStyle(
-                      color: Color(0xFF545454),
-                      fontSize: 37,
-                    ),
-                  ),
-                  Image.asset("img/elderly.jpg"),
-                  const Text(
-                    "كبير سن",
-                    style: TextStyle(
-                      color: Color(0xFF545454),
-                      fontSize: 20,
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  Image.asset("img/cargiver.jpg"),
-                  const Text(
-                    "مقدم رعاية",
-                    style: TextStyle(
-                      color: Color(0xFF545454),
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "من أنت ؟",
+                textAlign: TextAlign.right,
+                textDirection: TextDirection.rtl,
+                style: TextStyle(
+                  color: Color(0xFF545454),
+                  fontSize: 37,
+                ),
               ),
-            ),
-          ],
+              ClipOval(
+                child: Image.asset(
+                  "img/elderly.jpg",
+                  width: 150, // Adjust as needed
+                  height: 150, // Adjust as needed
+                ),
+              ),
+              const Text(
+                "كبير سن",
+                style: TextStyle(
+                  color: Color(0xFF545454),
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 50),
+              ClipOval(
+                child: Image.asset(
+                  "img/cargiver.jpg",
+                  width: 150, // Adjust as needed
+                  height: 150, // Adjust as needed
+                ),
+              ),
+              const Text(
+                "مقدم رعاية",
+                style: TextStyle(
+                  color: Color(0xFF545454),
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
